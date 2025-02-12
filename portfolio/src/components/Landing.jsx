165f87da605b1a7e0ae139aae359acf20e918799
Arshia Talwar <arshiatalwar17@gmail.com>
2025-02-12 21:48:58 +0530

    set mobile menu

--- portfolio/src/components/Landing.jsx
+++ portfolio/src/components/Landing.jsx
@@ -1,6 +1,8 @@
 
 import { CommandLineIcon, CodeBracketSquareIcon,ComputerDesktopIcon,UserGroupIcon } from '@heroicons/react/24/outline'
 import { useEffect, useMemo, useState } from "react";
+import { Dialog, DialogPanel } from '@headlessui/react'
+import {  XMarkIcon } from '@heroicons/react/24/outline'
 import { motion } from "framer-motion";
 import Squares from './Background';
 
@@ -39,10 +41,11 @@ const features = [
   },
 ]
 export default function Landing() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     const [titleNumber, setTitleNumber] = useState(0);
     const titles = useMemo(
-      () => ["DEVELOPER", "DESIGNER", "YAPPER", "EXPLORER", "DREAMER"]
+      () => ["DEVELOPER", "DESIGNER", "YAPPER", "EXPLORER", "HUSTLER"]
     
     );
   
@@ -90,7 +93,53 @@ export default function Landing() {
             </a>
           </div>
         </nav>
-       
+         {/* Mobile menu, show/hide based on mobile menu state. */}
+        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  alt=""
+                  src=""
+                  className="h-8 w-auto"
+                />
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="mailto:arshiatalwar17@gamil.com"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Hire me
+                  </a>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
       </header>
 
       <div className="relative isolate px-6  lg:px-8">
@@ -112,7 +161,7 @@ export default function Landing() {
               <div className=" intro flex-1 ml-10 mr-80">
                 
             <h1 className=" text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-              Hi! I'm Arshia Talwar
+              Hi! I am Arshia Talwar
             </h1>
             <p className=" mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-3xl font-mono">
              I'm a
